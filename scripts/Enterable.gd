578bd0e17a307624e85592a4066d1b4bde2958d9
jkamins7 <jkaminsky@jhu.edu>
2024-08-16 12:10:04 -1000

    Got entering and exiting tiny levels working

--- scripts/Enterable.gd
+++ scripts/Enterable.gd
@@ -1,10 +1,15 @@
 extends Area2D
 @onready var outer_parent = get_parent().get_parent().get_parent()
 @onready var outer_scene = get_parent().get_parent()
-@onready var inner_parent = get_parent().get_parent()
-@onready var inner_scene = get_parent()
+@onready var inner_parent = get_parent()
+@onready var inner_scene = self
 @onready var previous_level = null
 @export var entered: bool = false
+
+
+func _change_parent(object, new_parent) -> void:
+	object.get_parent().remove_child(object)
+	new_parent.add_child(object)
 
 func _set_current_level(current, proposed, body) -> void:
 	var root = current.get_parent()
@@ -12,28 +17,27 @@ func _set_current_level(current, proposed, body) -> void:
 		proposed.get_parent().remove_child(proposed)
 	current.get_parent().remove_child(current)
 	root.add_child(proposed)
-	current.remove_child(body)
-	proposed.add_child(body)
+	_change_parent(body, proposed)
 
 func _get_current_level() -> Node:
 	var root := get_tree().root
 	return root.get_child(0)
 
 func enter(body: CharacterBody2D) -> void:
-	if not entered:
-		_set_current_level(_get_current_level(), inner_scene, body)
-		entered = true
+	_set_current_level(_get_current_level(), inner_scene, body)
+	entered = true
 
 func exit(body: CharacterBody2D) -> void:
-	if entered:
-		_set_current_level(_get_current_level(), outer_scene, body)
-		entered = false
+	_set_current_level(_get_current_level(), outer_scene, body)
+	inner_parent.add_child(inner_scene)
+	entered = false
 
 # SIGNALS
 
 func _on_body_entered(body):
-	call_deferred("enter", body)
+	if not entered:
+		call_deferred("enter", body)
 
 func _on_body_exited(body):
-	# call_deferred("exit", body)
-	pass
+	if entered:
+		call_deferred("exit", body)
