861f9045a399bc9805a3d1d43f06c61322722583
Kenny Denmark <denmark1@umbc.edu>
2024-08-16 20:55:10 -0400

    added basic tile art and player character

--- scripts/Player2D.gd
+++ scripts/Player2D.gd
@@ -4,21 +4,49 @@ extends CharacterBody2D
 
 @onready var camera: Camera2D = $Camera
 
-const SPEED: float = 5.0 * 16
-const JUMP_VELOCITY: float = 15 * 16
 
-func _physics_process(_delta: float) -> void:
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction: -1, 0, 1
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	# Flip the Sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("walk")
+	else:
+		animated_sprite.play("jump")
+	
+	# Apply movement
 	if direction:
-		velocity = SPEED * direction
+		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
 
-	var _collision: bool = move_and_slide()
+	move_and_slide()
 	
 func enable_camera() -> void:
 	pass
