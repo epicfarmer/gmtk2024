2b340176b0afdb836f51b0eddbb34a1734bf0508
Kenny Denmark <denmark1@umbc.edu>
2024-08-17 14:55:33 -0400

    added outer boundary

--- scripts/exit_boundary.gd
+++ scripts/exit_boundary.gd
@@ -9,7 +9,16 @@ extends Node2D
 @onready var right = $right
 
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-#	down.position = Vector2((extents.x - origin.x) / 2, extents.y )
-#	down.emission_rect_extents = Vector2(extents.x - origin.x)
+func _ready():
+	down.position = Vector2((extents.x + origin.x)/2, extents.y )
+	down.emission_rect_extents = Vector2((extents.x - origin.x)/2 , 1)
+	
+	up.position = Vector2((extents.x + origin.x)/2 , origin.y )
+	up.emission_rect_extents = Vector2((extents.x - origin.x)/2 , 1)
+	
+	left.position = Vector2(origin.x, (extents.y + origin.y)/2 )
+	left.emission_rect_extents = Vector2(1, (extents.y - origin.y)/2)
+	
+	right.position = Vector2(extents.x, (extents.y + origin.y)/2 )
+	right.emission_rect_extents = Vector2(1,  (extents.y - origin.y)/2)
 
